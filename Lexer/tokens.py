1cec48b6180b73ac24a198f407495e64ad3fd1e5
Apollo13-0 <calderon.ig08@estudiantec.cr>
2022-09-01 19:58:13 -0600

    add tokens and regular exp

--- Lexer/tokens.py
+++ Lexer/tokens.py
@@ -9,16 +9,18 @@ tokens = {
 
     # Reserved words
     "NEW",
-    "IF",
-    "ELSE",
-    "FOR",
-    "TO",
-    "STEP",
-    "SET",
-    "PRINT",
-    "DEF",
-    "NUM",
-    "BOOL",
+    "PROC", "PRINCIPAL",
+    "CALL", "NUM", "BOOL",
+    "VALUES",
+    "ALTER", "ADD", "SUB", "MUL", "DIV",
+    "ALTERB",
+    "MOVERIGHT", "MOVELEFT", "HAMMER", "STOP",
+    "ISTRUE",
+    "REPEAT",
+    "UNTIL", "WHILE",
+    "CASE", "WHEN", "ELSE",
+    "PRINTVALUES",
+
 
     # Operators
     "PLUS",
@@ -27,10 +29,11 @@ tokens = {
     "INT_DIVIDE",
     "DIVIDE",
     "MODULE",
-    "EQUAL",
-    "NOT_EQUAL"
 
-    # Assigment
+    # <, <=,  >, >=, ==, <>
+    'LT', 'LE', 'GT', 'GE', "EQUAL", "NOT_EQUAL"
+
+    # Assigment =
     "ASSING",
 
     # Bracket ( ) { } , . ; :
@@ -48,9 +51,22 @@ tokens = {
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
-t_DIVIDE = r'/'
 t_INT_DIVIDE = r'//'
+t_DIVIDE = r'/'
 t_MODULE = r'%'
+
+# Assigment
 t_EQUAL = r'=='
+
+# Bracket
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+t_LBRACE = r"\{"
+t_RBRACE = r"\}"
+t_COMMA = r"\,"
+t_DOT = r"\."
+t_SEMICOLON = r"\;"
+t_COLON = r"\:"
+
+
+t_ignore = ' \t'
